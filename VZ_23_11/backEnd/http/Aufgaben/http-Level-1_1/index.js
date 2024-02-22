0b546345bfa286524565cb538d167a84f3c5de13
Kaiznkinkaide <daniel.th.cohen@gmail.com>
2024-02-22 15:33:29 +0100

    http

---
+++ VZ_23_11/backEnd/http/Aufgaben/http-Level-1_1/index.js
@@ -0,0 +1,32 @@
+import http from 'http'
+import fs from 'fs'
+
+
+const sendFile = (name, res) =>{
+    fs.readFile(`./${name}`, (err, data)=>{
+        if(err){
+            res.writeHead(500);
+            res.end("Server Error");
+        }else{
+            res.end(data)
+        }
+
+})}
+
+
+
+const server = http.createServer((req, res) =>{
+    if(req.url === '/'){
+        sendFile("index.html", res)
+    } else if(req.url === '/about'){
+        sendFile("about.html", res)
+    } else if (req.url === '/contact'){
+        sendFile("contact.html", res)
+    } else if(req.url === '/cock')
+        sendFile("cock.html", res)}
+)
+
+
+server.listen(2000, () =>{
+    console.log("listining on port http://localhost:5000");
+})
